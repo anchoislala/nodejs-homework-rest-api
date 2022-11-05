5e27253b264adc2a5127d842c66595aacd4ef671
Anna Leszka <ania.lalala97@gmail.com>
2022-11-05 21:35:03 +0100

    homework 3

---
+++ controllers/contacts/getAllContacts.js
@@ -0,0 +1,10 @@
+const { Contact } = require('../../models/contact');
+
+const getAllContacts = async (req, res, next) => {
+
+  const result = await Contact.find();
+  res.json(result);
+
+};
+
+module.exports = getAllContacts;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,21 @@
-const express = require('express')
-
+const express = require('express');
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const ctrl = require('../../controllers/contacts');
+const { ctrlWrapper } = require('../../helpers');
+const { validateBody } = require('../../middlewares');
+const { schemas } = require('../../models/contact');
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', ctrlWrapper(ctrl.getAllContacts))
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:contactId', ctrlWrapper(ctrl.getById))
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', validateBody(schemas.addSchema), ctrlWrapper(ctrl.add))
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:contactId', ctrlWrapper(ctrl.removeById))
+
+router.put('/:contactId', validateBody(schemas.addSchema), ctrlWrapper(ctrl.updateById))
+
+router.patch('/:contactId/favorite', validateBody(schemas.updateFavoriteSchema), ctrlWrapper(ctrl.updateStatusContact))
 
 module.exports = router
