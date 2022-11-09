b82e58642163e19f818881e65d7072c7fa81519c
Anna Leszka <ania.lalala97@gmail.com>
2022-11-09 15:32:31 +0100

    homework 4

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,19 +3,19 @@ const router = express.Router()
 
 const ctrl = require('../../controllers/contacts');
 const { ctrlWrapper } = require('../../helpers');
-const { validateBody } = require('../../middlewares');
+const { validateBody, authenticate } = require('../../middlewares');
 const { schemas } = require('../../models/contact');
 
-router.get('/', ctrlWrapper(ctrl.getAllContacts))
+router.get('/', authenticate, ctrlWrapper(ctrl.getAllContacts))
 
-router.get('/:contactId', ctrlWrapper(ctrl.getById))
+router.get('/:contactId', authenticate, ctrlWrapper(ctrl.getById))
 
-router.post('/', validateBody(schemas.addSchema), ctrlWrapper(ctrl.add))
+router.post('/', authenticate, validateBody(schemas.addSchema), ctrlWrapper(ctrl.add))
 
-router.delete('/:contactId', ctrlWrapper(ctrl.removeById))
+router.delete('/:contactId', authenticate, ctrlWrapper(ctrl.removeById))
 
-router.put('/:contactId', validateBody(schemas.addSchema), ctrlWrapper(ctrl.updateById))
+router.put('/:contactId', authenticate, validateBody(schemas.addSchema), ctrlWrapper(ctrl.updateById))
 
-router.patch('/:contactId/favorite', validateBody(schemas.updateFavoriteSchema), ctrlWrapper(ctrl.updateStatusContact))
+router.patch('/:contactId/favorite', authenticate, validateBody(schemas.updateFavoriteSchema), ctrlWrapper(ctrl.updateStatusContact))
 
-module.exports = router
+module.exports = router;
